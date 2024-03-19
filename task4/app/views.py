99ca01ef62f570cfcb81450376d3b279418e0e4f
MahendraSurisetty <84585646+MahendraSurisetty@users.noreply.github.com>
2024-03-19 17:24:21 +0530

    Add files via upload

---
+++ task4/app/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+from rest_framework import generics
+from .models import Category
+from .serializers import CategorySerializer
+
+class CategoryList(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
